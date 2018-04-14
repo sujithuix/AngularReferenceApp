f4d7085683eea2762ec6b0cd34d906c9d5db2bb7
SJ Sujith <sujith777@gmail.com>
2018-04-14 19:15:23 +0530

    Daily merge
    1st draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TEK Tutorials HUB';
+  title = 'Angular Reference App';
 
  
   Counter:number = 5;
