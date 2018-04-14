7c20435defbe6b3ab49302829bfbfb0caa0b44b1
SJ Sujith <sujithuix@gmail.com>
2018-04-15 01:47:22 +0530

    data binding 1st revision final commit
    more comments should be added later

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -12,16 +12,34 @@ export class DataBindingComponent implements OnInit {
   ngOnInit() {
   }
 
-text:string = "Binding";
+  // Data binding
+  //interpolation
 
-getTitle():string {
- return this.text;
-}
-
-  carMake:string = "BMW";
-  carModel:string = "M5";
+  text: string = "Binding";
 
   // 'text' is called property
   // The value of 'text' which is passed to teh template is 'Binding'
 
+  getTitle(): string {
+    return this.text;
+  }
+  carMake: string = "BMW";
+  carModel: string = "M5";
+
+  color: string = "#e88e1a";
+
+// event and property binding binding
+
+  clickCount: number = 0;
+
+  clickCounter() {
+    this.clickCount = this.clickCount+1;
+    console.log('clicked')
+  }
+
+  // Two way binding using ng-model
+  
+   formFeildValue: string ="";
+   
+   
 }
